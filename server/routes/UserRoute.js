c1aa14da88f9b6fcc4980156db8f7bd509f60155
Azer <imranovazer@gmail.com>
2023-07-12 18:27:26 +0400

    changes

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/UserController");
-
+const Middelware = require("../middleware/middelware");
 const router = express.Router();
 
 router.route("/register").post(userController.register);
 
 router.route("/login").post(userController.login);
+
+router.route("/who-am-i").get(Middelware.protect, userController.whoAmI);
 module.exports = router;
