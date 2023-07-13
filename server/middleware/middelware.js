e00696581e2b3a25f231f24e4885301e7a65aa9c
Azer <imranovazer@gmail.com>
2023-07-13 14:44:43 +0400

    changes

--- server/middleware/middelware.js
+++ server/middleware/middelware.js
@@ -2,34 +2,40 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 exports.protect = async (req, res, next) => {
-  let token;
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
+      if (!token) {
+        return res.status(403).json({
+          status: "fail",
+          message: "You are not authorized",
+        });
+      }
 
-    if (!token) {
+      const verify = await jwt.verify(token, process.env.SECRET_KEY);
+
+      const user = await User.findById(verify.id);
+      if (!user) {
+        return res.status(403).json({
+          status: "fail",
+          message: "You are not authorized",
+        });
+      }
+
+      req.user = user;
+      return next();
+    } else {
       return res.status(403).json({
         status: "fail",
         message: "You are not authorized",
       });
     }
-
-    const verify = await jwt.verify(token, process.env.SECRET_KEY);
-
-    const user = await User.findById(verify.id);
-    if (!user) {
-      return res.status(403).json({
-        status: "fail",
-        message: "You are not authorized",
-      });
-    }
-
-    req.user = user;
-    return next();
-  } else {
+  } catch (error) {
     return res.status(403).json({
       status: "fail",
       message: "You are not authorized",
